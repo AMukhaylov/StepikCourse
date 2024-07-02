992c1575f0ccdf947cb40e03453e1240039be525
AMukhaylov <arthurcloud@yandex.ru>
2024-07-03 00:21:50 +0500

    Initial commit

---
+++ lesson_5/find_lesson.py
@@ -0,0 +1,15 @@
+import time
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://freeconferencecall.com/login")
+
+driver.find_element("id", "loginformsubmit").click()
+
+time.sleep(3)
